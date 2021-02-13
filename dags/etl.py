6fafd620fceead68aa1fbefd44da740185732ebf
Marcus Zeug <zeug.marcus@siemens.com>
2021-02-13 11:50:23 +0100

    feat: Include create_tables task

--- dags/etl.py
+++ dags/etl.py
@@ -2,8 +2,13 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
+from airflow.operators import (    
+    PostgresOperator,
+    StageToRedshiftOperator, 
+    LoadFactOperator,
+    LoadDimensionOperator, 
+    DataQualityOperator
+)
 from helpers import SqlQueries
 
 # AWS_KEY = os.environ.get('AWS_KEY')
@@ -25,7 +30,15 @@ dag = DAG('sparkify',
           catchup=False
         )
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
+
+## create all tables in redshift
+create_tables = PostgresOperator(
+    task_id='Create_tables',
+    dag=dag,
+    postgres_conn_id='redshift',
+    sql='create_tables.sql'
+)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
@@ -69,8 +82,9 @@ run_quality_checks = DataQualityOperator(
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-start_operator >> stage_events_to_redshift
-start_operator >> stage_songs_to_redshift
+start_operator >> create_tables
+create_tables >> stage_events_to_redshift
+create_tables >> stage_songs_to_redshift
 stage_events_to_redshift >> load_songplays_table
 stage_songs_to_redshift >> load_songplays_table
 load_songplays_table >> load_song_dimension_table
